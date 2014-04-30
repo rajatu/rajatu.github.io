9c17441fcc91a530f5c2edbdd0ca4dcf28a7b2a6
rajatu <rajatujawane@yahoo.com>
2014-04-30 22:59:13 +0530

    third commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login | Sign in</title>
+</head>
+<body>
+<form id="login_form "action="check_login.php" method="POST">
+Name:<input type="text" name="username"/>
+Password:<input type="password" name ="password"/>
+<input type="submit"/>
+</form>	
+<?php echo "under development"; ?>
+</body>
+</html>
